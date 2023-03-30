ce4ca5e3e6ba4ed8e7843fb29dd72da38b5bf1f7
Robert Sehr <robert.sehr@intranda.com>
2023-03-30 11:11:18 +0200

    bag submission

--- plugin/src/main/java/de/intranda/goobi/plugins/BagSubmissionStepPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/BagSubmissionStepPlugin.java
@@ -18,8 +18,13 @@
 
 package de.intranda.goobi.plugins;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 
+import org.apache.commons.configuration.SubnodeConfiguration;
+import org.goobi.beans.Process;
 import org.goobi.beans.Step;
 import org.goobi.production.enums.PluginGuiType;
 import org.goobi.production.enums.PluginReturnValue;
@@ -27,8 +32,21 @@ import org.goobi.production.enums.PluginType;
 import org.goobi.production.enums.StepReturnValue;
 import org.goobi.production.plugin.interfaces.IStepPluginVersion2;
 
-import lombok.Getter;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.SftpException;
 
+import de.sub.goobi.config.ConfigPlugins;
+import de.sub.goobi.helper.StorageProvider;
+import de.sub.goobi.helper.exceptions.SwapException;
+import de.sub.goobi.helper.transfer.SftpUtils;
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+import ugh.dl.DocStruct;
+import ugh.dl.Fileformat;
+import ugh.dl.Metadata;
+import ugh.exceptions.UGHException;
+
+@Log4j2
 public class BagSubmissionStepPlugin implements IStepPluginVersion2 {
 
     private static final long serialVersionUID = 2812883682084780289L;
@@ -38,24 +56,70 @@ public class BagSubmissionStepPlugin implements IStepPluginVersion2 {
     private Step step;
 
     @Getter
+    private Process process;
+
+    @Getter
     private String returnPath;
 
+    private String sftpUserName;
+    private String sftpPassword;
+    private String sftpHostname;
+    private String sftpPathToKnownHostsFile;
+    private int sftpPort = 22;
+    private String sftpRemoteFolder;
 
     @Override
     public PluginReturnValue run() {
-
+        String identifier = null;
         // open mets file, get doi
+        Path tarFile = null;
+        try {
+            Fileformat fileformat = process.readMetadataFile();
 
-        // check if zip/tar file exists
+            // find doi metadata
+            DocStruct ds = fileformat.getDigitalDocument().getLogicalDocStruct();
+            if (ds.getType().isAnchor()) {
+                ds = ds.getAllChildren().get(0);
+            }
+            for (Metadata md : ds.getAllMetadata()) {
+                if ("DOI".equals(md.getType().getName())) {
+                    identifier = md.getValue();
+                    break;
+                }
+            }
+            if (identifier == null) {
+                // no doi found, cancel
+                return PluginReturnValue.ERROR;
+            }
 
-        // sftp configuration
+            // check if tar file exists
 
-        // upload file
+            tarFile = Paths.get(process.getProcessDataDirectory(), identifier.replace("/", "_") + ".tar");
 
+            if (!StorageProvider.getInstance().isFileExists(tarFile)) {
+                // file not found, cancel
+                return PluginReturnValue.ERROR;
+            }
+
+        } catch (UGHException | IOException | SwapException e) {
+            log.error(e);
+        }
+        // open sftp connection
+        try (SftpUtils utils = new SftpUtils(sftpUserName, sftpPassword, sftpHostname, sftpPort, sftpPathToKnownHostsFile)) {
+            // upload file
+            utils.changeRemoteFolder(sftpRemoteFolder);
+            utils.uploadFile(tarFile);
+
+        } catch (JSchException | SftpException e) {
+            log.error(e);
+            return PluginReturnValue.ERROR;
+        }
         // delete local zip file
-
-
-
+        try {
+            StorageProvider.getInstance().deleteFile(tarFile);
+        } catch (IOException e) {
+            log.error(e);
+        }
 
         return PluginReturnValue.FINISH;
     }
@@ -84,10 +148,16 @@ public class BagSubmissionStepPlugin implements IStepPluginVersion2 {
     public void initialize(Step step, String returnPath) {
         this.returnPath = returnPath;
         this.step = step;
+        process = step.getProzess();
 
-        // read sftp configuration
+        SubnodeConfiguration myconfig = ConfigPlugins.getProjectAndStepConfig("intranda_step_bagcreation", step);
+        sftpUserName = myconfig.getString("/sftp/username");
+        sftpPassword = myconfig.getString("/sftp/password");
+        sftpHostname = myconfig.getString("/sftp/hostname");
+        sftpPathToKnownHostsFile = myconfig.getString("/sftp/knownHostsFile");
+        sftpPort = myconfig.getInt("/sftp/port");
+        sftpRemoteFolder = myconfig.getString("/sftp/remoteFolder");
     }
-
 
     @Override
     public PluginType getType() {
